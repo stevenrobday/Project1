1689e69c94ff0665d9a30a14ee30be2a877991ac
stevenrobday <stevenrobday@gmail.com>
2018-07-20 10:19:37 -0700

    Update app.js

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,3 +1,7 @@
+var utellyAPI = 'JFBfbDiSaimshBIIJ0KGCWkf3AyAp1jzmhCjsncKy5RpMzzONS';
+var tmdbAPI = 'a012a678bc4826e1cef39e62f3e9f471';
+var omdbAPI = '7e561c3d';
+
 M.AutoInit();
 
 $(document).ready(function () {
